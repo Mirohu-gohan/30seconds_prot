cd9fe47f8cbd93d676e4d27d4d771e4b41b29b2f
raytrek2-\ktc <KTC24A31A0043@edu.kyoto-tech.ac.jp>
2025-11-21 15:57:47 +0900

    Player参加画面Main画面

---
+++ Assets/kento/PlayerUI.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerUI : MonoBehaviour
+{
+    [SerializeField] private Text playerID;
+
+/*    public void SetCount(int count)
+    {
+        //playerID.text += $"Player {count}\n";
+        if (playerID != null)
+        {
+            playerID.text = "Player " + playerID.ToString();
+        }
+    }*/
+}

32f1b37815f2747c4f612208b7d825bb4f70bf79
raytrek2-\ktc <KTC24A31A0043@edu.kyoto-tech.ac.jp>
2026-01-09 15:36:39 +0900

    Playerブリンク

--- Assets/kento/Scripts/Player/brink/PlayerController1.cs
+++ Assets/kento/Scripts/Player/brink/PlayerController1.cs
@@ -1,4 +1,6 @@
-﻿
+﻿using System.Diagnostics.Contracts;
+using UnityEditor.Rendering;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
@@ -7,7 +9,7 @@ using UnityEngine.UI;
 public class PlayerController1 : MonoBehaviour
 {
     [Header("移動設定")]
-   
+
     [SerializeField] private float speed = 5.0f; //移動スピード
     [SerializeField] private float ChargeMoveSpeedRate = 0.3f; //チャージ・硬直中の速度倍率
     private float speed2 = 0; //チャージ中のスピード
@@ -22,13 +24,17 @@ public class PlayerController1 : MonoBehaviour
 
     [Header("ブリンク設定")]
 
-    [SerializeField] private float tackleForce = 15.0f;    //�^�b�N���p���[
-    [SerializeField] private float tackleDuration = 0.5f;//�^�b�N����Ԃ̎�������
-    [SerializeField] private float tackleCooldown = 1.0f;//�^�b�N���̃N�[���_�E������
-    [SerializeField] private float WeakKnockbackForce = 2.5f; //��p���`�m�b�N�o�b�N
-    [SerializeField] private float StrongKnockbackForce = 5.0f;//強パンチノックバック
+    [SerializeField] private float tackleForce;    //ブリンク力
+    //[SerializeField] private float tackleDuration = 0.5f;//持続時間
+    [SerializeField] private float tackleDuration = 5.0f;
+    [SerializeField] private float tackleCooldown = 1.0f;//クールダウン時間
+    [SerializeField] private float WeakKnockbackForce = 2.5f; //弱ブリンクノックバック
+    [SerializeField] private float StrongKnockbackForce = 5.0f;//強ブリンクノックバック
+
+    private Vector3 tackleStartPos;
+
     private float curentknockbackForce = 0f;//現在のノックバック力
-  
+
 
     [SerializeField] private float StrongRecoveryTime = 1.0f; //硬直時間
     private float curentRecoveryTime;
@@ -38,20 +44,19 @@ public class PlayerController1 : MonoBehaviour
     private Rigidbody rb;
     private bool isTackling = false;
     private float lastTackleTime = 0f; // 最後のタックル時間
-   
+
+
     private bool isPrese = false; //押されているかフラグ
     [HideInInspector] public bool isStrt = false;//タイマスタートフラグ
     private float t = 0f; //タイマー
     public float chargeMax = 5.0f; //タイマー上限
     private bool isMax = false;//チャージがMaxかのフラグ
-  
-   
+
+
 
     private int playerID;
     private PlayerInput playerInput;
     [SerializeField] private Text IDtext;
-
-    private Animator animator;
 
     //private float y = -5.0f;
 
@@ -62,45 +67,36 @@ public class PlayerController1 : MonoBehaviour
         rotSpeed2 = rotSpeed * ChargeRotateSpeedRate;
         curentRecoveryTime = StrongRecoveryTime;
     }
-    
+
 
     public void OnMove(InputAction.CallbackContext context)
     {
         inputVer = context.ReadValue<Vector2>();
     }
-    
+
     public void OnTackle(InputAction.CallbackContext context)
     {
         if (context.performed)
         {
             isfinish = false;
-            isPrese = true;
+
             if (!isTackling && Time.time > lastTackleTime + tackleCooldown)
             {
                 isStrt = true;
-                animator.SetBool("Charge", true);
+                isPrese = true;
             }
         }
-        if (context.canceled )
+        if (context.canceled)
         {
-            animator.SetBool("Charge", false);
             isPrese = false;
             if (isStrt && !isTackling && Time.time > lastTackleTime + tackleCooldown)
             {
                 Tackle();
-                if (isMax)
-                {
-                    animator.SetBool("AtackKyou", true);
-                }
-                else if (!isMax)
-                {
-                    animator.SetBool("AtackJaku", true);
-                }
             }
             isStrt = false;
         }
     }
-    
+
     public void SetCharge(float value)
     {
         t = value;
@@ -113,20 +109,25 @@ public class PlayerController1 : MonoBehaviour
         {
             playerID = playerInput.playerIndex;
         }
-        
+
         IDtext.text += $"Player {playerID + 1}\n";
-       
+
         rb = GetComponent<Rigidbody>();
-        animator = GetComponentInChildren<Animator>();
     }
 
     void FixedUpdate()
     {
-            Move();
-        if (!isfinish)
+        Move();
+
+        float mag = inputVer.magnitude;
+
+        if (isTackling)
         {
-            float mag = inputVer.magnitude;
-            animator.SetFloat("speed", mag);
+            float dist = Vector3.Distance(tackleStartPos, transform.position);
+            if (dist >= tackleDuration)
+            {
+                EndTackle();
+            }
         }
 
 
@@ -138,10 +139,10 @@ public class PlayerController1 : MonoBehaviour
             }
             if (t >= chargeMax)
             {
-              isMax = true;
+                isMax = true;
             }
         }
-        else if(!isStrt)
+        else if (!isStrt)
         {
             t = 0f;
         }
@@ -168,20 +169,21 @@ public class PlayerController1 : MonoBehaviour
             curentSpeed = speed2;
             curentRotSpeed = rotSpeed2;
         }
-        if(!isPrese)
+        if (!isPrese)
         {
             curentSpeed = speed;
             curentRotSpeed = rotSpeed;
         }
         Vector3 move = new Vector3(inputVer.x, 0f, inputVer.y) * curentSpeed * Time.deltaTime;
-        //rb.MovePosition(rb.position + move);
-        transform.position += move;
+        //transform.position += move;
+        rb.MovePosition(rb.position + move);
+
 
         if (move != Vector3.zero)
         {
             Quaternion Rot = Quaternion.LookRotation(move, Vector3.up);
-            transform.rotation = Quaternion.Slerp(transform.rotation,Rot,curentRotSpeed * Time.deltaTime);
-            //rb.MoveRotation(Quaternion.Slerp(rb.rotation, Rot, curentRotSpeed * Time.fixedDeltaTime));
+            //transform.rotation = Quaternion.Slerp(transform.rotation,Rot,curentRotSpeed * Time.deltaTime);
+            rb.MoveRotation(Quaternion.Slerp(rb.rotation, Rot, curentRotSpeed * Time.fixedDeltaTime));
         }
     }
 
@@ -191,12 +193,15 @@ public class PlayerController1 : MonoBehaviour
         isTackling = true;
         lastTackleTime = Time.time;
 
-        rb.AddForce(transform.forward * tackleForce * t, ForceMode.Impulse);
+        tackleStartPos = transform.position;
 
-        Invoke("EndTackle", tackleDuration);
+        rb.linearVelocity = Vector3.zero;
+        rb.AddForce(transform.forward * tackleForce, ForceMode.Impulse);
+
+        //Invoke("EndTackle", tackleDuration);
     }
 
-   
+
 
     void EndTackle()
     {
@@ -208,31 +213,36 @@ public class PlayerController1 : MonoBehaviour
         {
             isfinish = true;
         }
+
         isMax = false;
-        animator.SetBool("AtackKyou", false);
-        animator.SetBool("AtackJaku", false);
-
     }
-
 
     private void OnCollisionEnter(Collision collision)
     {
-        Rigidbody enemyrb = collision.gameObject.GetComponent<Rigidbody>();
-        if (enemyrb != null)
+        if(collision.gameObject.CompareTag("Player"))
         {
-            if (isMax)
+            if (isTackling)
             {
-                curentknockbackForce = StrongKnockbackForce;
-            }
-            else
-            {
-                curentknockbackForce = WeakKnockbackForce;
-            }
+                EndTackle();
 
-            Vector3 knockBackDir = collision.transform.position - transform.position;
-            knockBackDir.y = 0f;
-            Debug.Log(curentknockbackForce);
-            enemyrb.AddForce(knockBackDir.normalized * curentknockbackForce, ForceMode.Impulse);
+                Rigidbody enemyrb = collision.gameObject.GetComponent<Rigidbody>();
+                if (enemyrb != null && isTackling)
+                {
+                    if (isMax)
+                    {
+                        curentknockbackForce = StrongKnockbackForce;
+                    }
+                    else
+                    {
+                        curentknockbackForce = WeakKnockbackForce;
+                    }
+
+                    Vector3 knockBackDir = collision.transform.position - transform.position;
+                    knockBackDir.y = 0f;
+                    Debug.Log(curentknockbackForce);
+                    enemyrb.AddForce(knockBackDir.normalized * curentknockbackForce, ForceMode.Impulse);
+                }
+            }
         }
     }
 }

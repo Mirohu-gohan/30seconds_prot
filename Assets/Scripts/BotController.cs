0f9a4563e315bf387eadb5494ac0839f1e53e849
raytrek2-\ktc <>
2026-01-16 18:07:21 +0900

    ステージの新しいテクスチャ、隕石にぶつかったときのノックバック、低能ボットを導入。

---
+++ Assets/Scripts/BotController.cs
@@ -0,0 +1,101 @@
+using UnityEngine;
+
+public class BotController : MonoBehaviour
+{
+    [Header("移動設定")]
+    [SerializeField] private float Speed = 3f;
+    [SerializeField] private float rotate = 120f;
+
+    [Header("行動詳細")]
+    [SerializeField] private float minWaitTime = 1f;
+    [SerializeField] private float maxWaitTime = 3f;
+
+    [SerializeField] private float minWalkTime = 2f;
+    [SerializeField] private float maxWalkTime = 5f;
+
+    [Header("センサー")]
+    [SerializeField] private Transform Sensor;
+
+    [SerializeField] private float sensorRadius = 0.2f;
+    [SerializeField] private float sensorDistance = 0.5f;
+
+    [SerializeField] private LayerMask StageLayer;
+
+    private bool isMove = true;
+    private float actTimer = 0f;
+    private float nextActDuration = 0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        SetNextAct();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isGround = Check(Sensor);
+
+        if (!isGround)
+        {
+            Turn();
+            return;
+        }
+
+        actTimer += Time.deltaTime;
+
+        if (actTimer >= nextActDuration)
+        {
+            isMove = !isMove;
+            actTimer = 0f;
+            SetNextAct();
+        }
+
+        if (isMove)
+        {
+            transform.Translate(Vector3.forward * Speed * Time.deltaTime);
+        }
+    }
+
+
+    private bool Check(Transform sensor)
+    {
+        if (sensor == null)
+        {
+            return false;
+        }
+
+        RaycastHit hit;
+
+        if (Physics.SphereCast(sensor.position,sensorRadius,Vector3.down,out hit,sensorDistance,StageLayer))
+        {
+            return hit.collider.CompareTag("Stage");
+        }
+
+        return false;
+    }
+
+    private void Turn()
+    {
+        float rndAngle = Random.Range(90f, 180f);
+
+        if (Random.value > 0.5f)
+        {
+            rndAngle = -rndAngle;
+        }
+
+        transform.Rotate(0, rndAngle, 0);
+    }
+
+    private void SetNextAct()
+    {
+        if (isMove)
+        {
+            nextActDuration = Random.Range(minWalkTime, maxWalkTime);
+        }
+        else
+        {
+            nextActDuration = Random.Range(minWaitTime, maxWaitTime);
+        }
+    }
+}

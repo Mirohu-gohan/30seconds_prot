cd635aba3278417d31771d2d11da9c975e46b97e
Miwa <>
2026-05-08 19:20:45 +0900

    タイトルUIを実装しましたタイトルにて全体のBGMとSEの音をスライダーで調節できるようになりました

--- Assets/Miwa/scripts/Sound/VolumeController.cs
+++ Assets/Miwa/scripts/Sound/VolumeController.cs
@@ -1,21 +1,54 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class VolumeController : MonoBehaviour
 {
-
     public Slider bgmSlider;
     public Slider seSlider;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public TMP_Text bgmText;
+    public TMP_Text seText;
+
     void Start()
     {
-        if(SoundManager.Instance != null)
+        if (SoundManager.Instance != null)
         {
+            // 最初に現在の音量をスライダーとテキストに反映
             bgmSlider.value = SoundManager.Instance.bgmVolume;
             seSlider.value = SoundManager.Instance.seVolume;
 
-            bgmSlider.onValueChanged.AddListener(val => SoundManager.Instance.SEtBGMVolume(val));
-            seSlider.onValueChanged.AddListener(val => SoundManager.Instance.SetSEVolume(val));
+            UpdateBgmtext(bgmSlider.value);
+            UpdateSetext(seSlider.value);
+
+            // スライダーを動かした時の処理を登録
+            bgmSlider.onValueChanged.AddListener(val => 
+            {
+                SoundManager.Instance.SEtBGMVolume(val); // 音量を変える
+                UpdateBgmtext(val);                     // テキストを変える（←ここが抜けていました）
+            });
+
+            seSlider.onValueChanged.AddListener(val => 
+            {
+                SoundManager.Instance.SetSEVolume(val);  // 音量を変える
+                UpdateSetext(val);                      // テキストを変える（←ここが抜けていました）
+            });
+        }
+    }
+
+    void UpdateBgmtext(float value)
+    {
+        if (bgmText != null)
+        {
+            bgmText.text = Mathf.RoundToInt(value * 100).ToString();
+        }
+    }
+
+    void UpdateSetext(float value)
+    {
+        if (seText != null)
+        {
+            seText.text = Mathf.RoundToInt(value * 100).ToString();
         }
     }
 }
